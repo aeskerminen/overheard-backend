fc033cb4399d6977b5d516c1e653960c0e9e650a
aeskerm <kerminenartturi@gmail.com>
2024-07-02 15:41:26 +0300

    basic backend structure

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const app = express()
+const router = require('./controllers/routes')
+
+mongoose.set("strictQuery", false)
+mongoose.connect(config.URL)
+
+app.use(express.json())
+app.use(cors())
+
+app.use(morgan('tiny'))
+
+app.use("/api", router)
+
+
+module.exports = app
---
+++ utils/config.js
@@ -0,0 +1,5 @@
+let PORT = 3003
+let BASE_URL = "/api";
+let URL = "mongodb://localhost:27017/Overheard"
+
+module.exports = {PORT, BASE_URL, URL}
