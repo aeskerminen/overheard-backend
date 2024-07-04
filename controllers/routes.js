409a3686eade7797c6deae42cb58e774b2fc4842
aeskerm <kerminenartturi@gmail.com>
2024-07-04 15:38:13 +0300

    Missing return statements -_-

--- controllers/routes.js
+++ controllers/routes.js
@@ -11,27 +11,29 @@ router.post("/register", async (req, res) => {
 
     const exists = await User.find({email})
     if(exists) {
-        res.status(400).json({error: 'Email already in use...'})
+        return res.status(400).json({error: 'Email already in use...'})
     }
 
     const nUser = new User({email, password: hashedPass})
     nUser.save().then(result => {
         console.log(`User succesfully created: ${result}`)
-        res.status(201).json({message: 'User registered succesfully...'})
+        return res.status(201).json({message: 'User registered succesfully...'})
     }).catch(err => {
-        res.status(500).json({error: 'An error occurred when registering a new user...'})
+        return res.status(500).json({error: 'An error occurred when registering a new user...'})
     })
 })
 
 router.post("/login", async (req, res) => {
-    const {email, pass} = req.body;
+    const {email, password} = req.body;
     
     const user = await User.find({email})
     if(!user) {
-        res.status(401).json({error: 'Invalid user or password...'})
+        return res.status(401).json({error: 'Invalid user or password...'})
     }
 
-    if(await bcrypt.compare(pass, user.password)) {
+    console.log(user.password, password)
+
+    if(await bcrypt.compare(password, user.password)) {
         let payload = {
             email: user.email,
             id: user._id
@@ -39,9 +41,9 @@ router.post("/login", async (req, res) => {
 
         var token = jwt.sign(payload, process.env.SECRET, { algorithm: 'RS256', expiresIn: '1h' });
 
-        res.cookie('token', token, {httpOnly: true}).status(200).json({message: 'Logged in succesfully...'})
+        return res.cookie('token', token, {httpOnly: true}).status(200).json({message: 'Logged in succesfully...'})
     } else {
-        res.status(401).json({error: 'Invalid user or password...'})
+        return res.status(401).json({error: 'Invalid user or password...'})
     }
 })
 
