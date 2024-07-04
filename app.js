d09786e4cfc30b62323784f6a6a342d16babe76f
aeskerm <kerminenartturi@gmail.com>
2024-07-04 15:34:55 +0300

    Fix CORS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
@@ -6,18 +7,18 @@ const config = require('./utils/config.js')
 
 require('dotenv').config()
 
-const app = express()
 const router = require('./controllers/routes')
 
 mongoose.set("strictQuery", false)
 mongoose.connect(config.URL)
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin: 'http://localhost:5173', credentials: true}))
 
 app.use(morgan('tiny'))
 
 app.use("/api", router)
+
 
 
 module.exports = app
