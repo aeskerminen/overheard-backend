d7bed9b42dde5b1801140fc56d866d1374d9c09c
aeskerm <kerminenartturi@gmail.com>
2024-07-04 11:57:39 +0300

    fix imports

--- models/User.js
+++ models/User.js
@@ -1,9 +1,11 @@
+const mongoose = require('mongoose');
+
 const UserSchema = new mongoose.Schema({ 
     email: String,
     password: String,
     id: String
 }, {timestamps: true});
 
-const User = mongoose.model('User', UserSchema);v
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User
