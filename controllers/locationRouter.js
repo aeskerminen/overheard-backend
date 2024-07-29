5a7b6f5fd821ad5f02ba1ceb4fdbdb5d4d9a8a70
aeskerm <kerminenartturi@gmail.com>
2024-07-29 10:52:00 +0300

    add basic functionality

--- controllers/locationRouter.js
+++ controllers/locationRouter.js
@@ -1,3 +1,17 @@
 const locationRouter = require("express").Router();
+const axios = require("axios");
 
-locationRouter.post("/")
+locationRouter.post("/:lat/:lon", async (req, res) => {
+  const lat = req.params.lat;
+  const lon = req.params.lon;
+
+  const offgridResponse = await axios.post(
+    `localhost:8000/api/data/${lat}/${lon}`
+  );
+
+  console.log(offgridResponse);
+
+  return res.json(offgridResponse);
+});
+
+module.exports = locationRouter;
