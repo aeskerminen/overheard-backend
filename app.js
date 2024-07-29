4d15f3b0787f4f2376976a9c77a24fade8a0838a
aeskerm <kerminenartturi@gmail.com>
2024-07-29 10:49:58 +0300

    add router for location

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const morgan = require('morgan')
-const cookieParser = require('cookie-parser')
-const mongoose = require('mongoose')
-const config = require('./utils/config.js')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const config = require("./utils/config.js");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const router = require('./controllers/postRouter.js')
-const postRouter = require('./controllers/postRouter.js')
-const userRouter = require('./controllers/userRouter.js')
+const router = require("./controllers/postRouter.js");
+const postRouter = require("./controllers/postRouter.js");
+const userRouter = require("./controllers/userRouter.js");
+const locationRouter = require("./controllers/locationRouter.js");
 
-mongoose.set("strictQuery", false)
-mongoose.connect(config.URL)
+mongoose.set("strictQuery", false);
+mongoose.connect(config.URL);
 
-app.use(express.json())
-app.use(cors({origin: 'http://localhost:5173', credentials: true}))
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(morgan('tiny'))
+app.use(morgan("tiny"));
 
-app.use("/api/posts", postRouter)
-app.use("/api/users", userRouter)
+app.use("/api/posts", postRouter);
+app.use("/api/users", userRouter);
+app.use("/api/location", locationRouter);
 
-module.exports = app
+module.exports = app;
---
+++ controllers/locationRouter.js
@@ -0,0 +1,3 @@
+const locationRouter = require("express").Router();
+
+locationRouter.post("/")
